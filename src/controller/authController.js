0cf89ddd573d268a41e643b079bc38a0c2bf7ce0
lijinlawrence <kittulawrence@gmail.com>
2024-09-16 11:51:46 +0530

    profile image error

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -13,6 +13,7 @@ export const registerUser = catchAsyncError(async (req, res, next) => {
   let BASE_URL = process.env.BACKEND_URL;
   if(process.env.NODE_ENV === "production"){
     BASE_URL = `${req.protocol}://${req.get('host')}`
+    console.log(BASE_URL); 
   }
 
   if (req.file) {
